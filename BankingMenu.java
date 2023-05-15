c9393aa81b6d146b1f0ff73da7cdab9cb1cf5cad
Nayab Sunanda <104186816+sunandanayab@users.noreply.github.com>
2023-05-15 22:07:37 +0530

    Add files via upload

---
+++ BankingMenu.java
@@ -0,0 +1,20 @@
+//BankingMenu.java
+package bm;
+public class BankingMenu 
+{
+	public static void menu()
+    {
+		System.out.println("------------------------------------");
+        System.out.println("\t\tWELCOME TO ATM APPLICATION");
+		System.out.println("------------------------------------");
+        System.out.println("\t\tSelect Your Choice");
+		System.out.println("------------------------------------");
+        System.out.println("\t\t1. Deposit");
+        System.out.println("\t\t2. Cash WithDrawal");
+        System.out.println("\t\t3. Balance Enquiry");
+        System.out.println("\t\t4. Exit");
+		System.out.println("------------------------------------");
+
+
+    }
+}
